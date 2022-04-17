3593842fb620a1be7cad555d34a241f1f9c27c68
adolenec <andrija.dolenec23@gmail.com>
2022-04-17 17:52:09 +0200

    Add http request for login and sign up

--- src/components/authentication/AuthForm.js
+++ src/components/authentication/AuthForm.js
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import classes from "./AuthForm.module.css";
 import addressBg from "../../assets/addressBook.webp";
+import { useHistory } from 'react-router-dom';
 
 import useInput from "../../hooks/useInput";
 
 const AuthForm = (props) => {
-
+    const [errorMsg, setErrorMsg] = useState('');
     const [isLogin, setIsLogin] = useState(true);
+    const history = useHistory();
 
   //email
   const {
@@ -49,12 +51,56 @@ const AuthForm = (props) => {
           return;
       }
 
+      let url;
+      let msg;
+
+      if(isLogin) {
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAuPyU-vTstxsbdjpRKaEc9tGcU2WiwEFQ';
+      }else {
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAuPyU-vTstxsbdjpRKaEc9tGcU2WiwEFQ';
+      }
+      fetch(url, {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+          }),
+          headers: {
+              'Content-type' : 'application/json'
+          }
+      }).then(res => {
+          if(res.ok){
+              return res.json().then(data=>{
+                  setErrorMsg('');
+              });
+          } else {
+              return res.json().then(data => {
+                console.log(data);
+                if(data.error.message === 'INVALID_PASSWORD') {
+                    msg = "Invalid password";
+                }else if(data.error.message === 'EMAIL_EXISTS'){
+                    msg = "Email already exists";
+                }else{
+                    msg = "This e-mail does not exist";
+                }
+                throw new Error(msg);
+              })
+          }
+      }).then(data => {
+          //here save token in redux store later
+          history.replace('/adresar') 
+      }).catch(errorMsg => {
+          setErrorMsg(errorMsg.message);
+      })
+
       resetEmailInput();
       resetPasswordInput();
   }
 
   const changeAuth = () => {
     setIsLogin((prevState) => !prevState);
+    setErrorMsg('');
   }
 
   return (
@@ -66,7 +112,7 @@ const AuthForm = (props) => {
       <div className={classes["form-container"]}>
         <form className={classes.form} onSubmit={formSubmitHandler}>
           <div className={classes["form-control"]}>
-            <label for="email">Email</label>
+            <label htmlFor="email">Email</label>
             <input
               type="email"
               id="email"
@@ -74,10 +120,10 @@ const AuthForm = (props) => {
               onBlur={emailBlurHandler}
               value={enteredEmail}
             />
-            {emailInputHasError && <p>Please enter a valid email address</p>}
+            {emailInputHasError && <p className={classes['error-msg']}>Please enter a valid email address</p>}
           </div>
           <div className={classes["form-control"]}>
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input
               type="password"
               id="password"
@@ -85,7 +131,7 @@ const AuthForm = (props) => {
               onBlur={passwordBlurHandler}
               value={enteredPassword}
             />
-            {passwordInputHasError && <p>Password must contain at least one number and one special character</p>}
+            {passwordInputHasError && <p className={classes['error-msg']}>Password must be at least 6 characters long and contain at least one number and one special character</p>}
           </div>
           <div className={classes["btn-container"]}>
             <button disabled={!formIsValid}>{isLogin ? 'Login' : 'Sign up'}</button>
@@ -93,6 +139,7 @@ const AuthForm = (props) => {
           <button type="button" className={classes["switchBtn"]} onClick={changeAuth}>
             {isLogin ? 'Don\'t have an account? Sign up for free' : 'Already have an account? Login'}
           </button>
+          <p className={classes['error-msg']}>{errorMsg}</p>
         </form>
       </div>
     </section>
