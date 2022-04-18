4126c90ab70d9fb9032d49f790d0084f61e7ec48
adolenec <andrija.dolenec23@gmail.com>
2022-04-18 10:22:40 +0200

    Add NewContact form component

--- src/components/Contacts/NewContact.js
+++ src/components/Contacts/NewContact.js
@@ -1,5 +1,40 @@
-const NewContact = () => {
+import classes from './NewContact.module.css';
 
+const NewContact = () => {
+    return(
+        <section className={classes['add-contact-form']}>
+            <div className={classes['form-info']}>
+                <i className="fa-solid fa-user fa-10x"></i>
+                <h2>Add new user</h2>
+            </div>
+            <form className={classes.form}>
+                <div className={classes['form-control']}>
+                    <label htmlFor='name'>Name</label>
+                    <input type="text" id="name"/>
+                </div>
+                <div className={classes['form-control']}>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input type="text" id="lastName"/>
+                </div>
+                <div className={classes['form-control']}>
+                    <label htmlFor='date'>Date of Birth</label>
+                    <input type="date" id="date"/>
+                </div>
+                <div className={classes['form-control']}>
+                    <label htmlFor='contactType'>Contact Type</label>
+                    <select id='contactType' name='contactType'>
+                        <option value="mobile phone">Mobile phone</option>
+                        <option value="telephone">Telephone</option>
+                        <option value="email">Email</option>
+                        <option value="pager">Pager</option>
+                    </select>
+                </div>
+                <div className={classes['submit-btn']}>
+                    <button>Add New Contact</button>
+                </div>
+            </form>
+        </section>
+    )
 }
 
 export default NewContact;
--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,20 +1,34 @@
-import { NavLink } from 'react-router-dom';
-import classes from './Navigation.module.css'
+import { NavLink } from "react-router-dom";
+import classes from "./Navigation.module.css";
 
 const Navigation = () => {
-    return (
-        <header className={classes.header}>
-            <h1>My Address Book</h1>
-            <nav>
-                <ul>
-                    <li><input type="search" placeholder='Search...'/></li>
-                    <li><NavLink activeClassName={classes.active} to="/kontakt"><i className="fa-solid fa-user"></i> New Contact</NavLink></li>
-                    <li><NavLink activeClassName={classes.active} to="/adresar" exact><i className="fa-solid fa-user-group"></i> My Contacts</NavLink></li>
-                    <li><NavLink activeClassName={classes.active} to="/adresar/omiljeni"><i className="fa-solid fa-star"></i> Favourites</NavLink></li>
-                </ul>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header className={classes.header}>
+      <h1>My Address Book</h1>
+      <nav>
+        <ul>
+          <li>
+            <input type="search" placeholder="Search..." />
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/kontakt">
+            <i className="fa-solid fa-user-plus"></i> New Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/adresar" exact>
+              <i className="fa-solid fa-user-group"></i> My Contacts
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName={classes.active} to="/adresar/omiljeni">
+              <i className="fa-solid fa-star"></i> Favourites
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navigation;
