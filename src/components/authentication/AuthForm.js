f6165c5bb4592274c8af2363def38c628d72bfca
adolenec <andrija.dolenec23@gmail.com>
2022-04-23 20:37:23 +0200

    Remove redundant comment and fix submit check

--- src/components/authentication/AuthForm.js
+++ src/components/authentication/AuthForm.js
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+import { useState } from "react";
 import classes from "./AuthForm.module.css";
 import addressBg from "../../assets/addressBook.webp";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 import useInput from "../../hooks/useInput";
 
 const AuthForm = () => {
-    const [errorMsg, setErrorMsg] = useState('');
-    const [isLogin, setIsLogin] = useState(true);
-    const history = useHistory();
+  const [errorMsg, setErrorMsg] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
+  const history = useHistory();
 
-    const validEmailFormat = /^\S+@\S+\.\S{2}/
+  const validEmailFormat = /^\S+@\S+\.\S{2}/;
   //email
   const {
     value: enteredEmail,
@@ -18,7 +18,7 @@ const AuthForm = () => {
     hasError: emailInputHasError,
     inputChangeHandler: emailChangeHandler,
     inputBlurHandler: emailBlurHandler,
-    resetInputs: resetEmailInput
+    resetInputs: resetEmailInput,
   } = useInput((value) => value.match(validEmailFormat));
 
   //password
@@ -30,7 +30,7 @@ const AuthForm = () => {
     hasError: passwordInputHasError,
     inputChangeHandler: passwordChangeHandler,
     inputBlurHandler: passwordBlurHandler,
-    resetInputs: resetPasswordInput
+    resetInputs: resetPasswordInput,
   } = useInput(
     (value) =>
       value.trim().length > 5 &&
@@ -38,66 +38,69 @@ const AuthForm = () => {
       specialChars.test(value)
   );
 
-  //overall form validity
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
 
-  const formSubmitHandler = e => {
-      e.preventDefault();
+    if (!emailIsValid || !passwordIsValid) {
+      return;
+    }
 
-      if(!emailIsValid && !passwordIsValid){
-          return;
-      }
+    let msg;
 
-      let msg;
+    const url = isLogin
+      ? "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAuPyU-vTstxsbdjpRKaEc9tGcU2WiwEFQ"
+      : "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAuPyU-vTstxsbdjpRKaEc9tGcU2WiwEFQ";
 
-      const url = isLogin ? 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAuPyU-vTstxsbdjpRKaEc9tGcU2WiwEFQ' :  'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAuPyU-vTstxsbdjpRKaEc9tGcU2WiwEFQ';
-
-      fetch(url, {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true
-          }),
-          headers: {
-              'Content-type' : 'application/json'
-          }
-      }).then(res => {
-          if(res.ok){
-              return res.json().then(data => {
-                  setErrorMsg('');
-              });
-          } else {
-              return res.json().then(data => {
-                if (data.error.message === 'INVALID_PASSWORD') {
-                    msg = "Invalid password";
-                } else if (data.error.message === 'EMAIL_EXISTS'){
-                    msg = "Email already exists";
-                } else if (data.error.message === 'INVALID_EMAIL'){
-                  msg = "Email is invalid!";
-                } else {
-                    msg = "This e-mail does not exist";
-                }
-                throw new Error(msg);
-              })
-          }
-      }).then(data => {
-          //here save token in redux store later
-          history.replace('/adresar') 
-      }).catch(errorMsg => {
-        console.log(errorMsg);
-          setErrorMsg(errorMsg.message);
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json().then((data) => {
+            setErrorMsg("");
+          });
+        } else {
+          return res.json().then((data) => {
+            if (data.error.message === "INVALID_PASSWORD") {
+              msg = "Invalid password";
+            } else if (data.error.message === "EMAIL_EXISTS") {
+              msg = "Email already exists";
+            } else if (data.error.message === "INVALID_EMAIL") {
+              msg = "Email is invalid!";
+            } else {
+              msg = "This e-mail does not exist";
+            }
+            throw new Error(msg);
+          });
+        }
       })
+      .then((data) => {
+        //here save token in redux store later
+        history.replace("/adresar");
+      })
+      .catch((errorMsg) => {
+        console.log(errorMsg);
+        setErrorMsg(errorMsg.message);
+      });
 
-      resetEmailInput();
-      resetPasswordInput();
-  }
+    resetEmailInput();
+    resetPasswordInput();
+  };
 
   const changeAuth = () => {
     setIsLogin((prevState) => !prevState);
-    setErrorMsg('');
+    setErrorMsg("");
     resetEmailInput();
     resetPasswordInput();
-  }
+  };
 
   return (
     <section className={classes["form-page"]}>
@@ -117,7 +120,11 @@ const AuthForm = () => {
               onBlur={emailBlurHandler}
               value={enteredEmail}
             />
-            {emailInputHasError && <p className={classes['error-msg']}>Please enter a valid email address</p>}
+            {emailInputHasError && (
+              <p className={classes["error-msg"]}>
+                Please enter a valid email address
+              </p>
+            )}
           </div>
           <div className={classes["form-control"]}>
             <label htmlFor="password">Password</label>
@@ -129,15 +136,26 @@ const AuthForm = () => {
               onBlur={passwordBlurHandler}
               value={enteredPassword}
             />
-            {passwordInputHasError && <p className={classes['error-msg']}>Password must be at least 6 characters long and contain at least one number and one special character</p>}
+            {passwordInputHasError && (
+              <p className={classes["error-msg"]}>
+                Password must be at least 6 characters long and contain at least
+                one number and one special character
+              </p>
+            )}
           </div>
           <div className={classes["btn-container"]}>
-            <button>{isLogin ? 'Login' : 'Sign up'}</button>
+            <button>{isLogin ? "Login" : "Sign up"}</button>
           </div>
-          <button type="button" className={classes["switchBtn"]} onClick={changeAuth}>
-            {isLogin ? `Don't have an account? Sign up for free` : `Already have an account? Login`}
+          <button
+            type="button"
+            className={classes["switchBtn"]}
+            onClick={changeAuth}
+          >
+            {isLogin
+              ? `Don't have an account? Sign up for free`
+              : `Already have an account? Login`}
           </button>
-          {errorMsg && <p className={classes['error-msg']}>{errorMsg}</p>}
+          {errorMsg && <p className={classes["error-msg"]}>{errorMsg}</p>}
         </form>
       </div>
     </section>
