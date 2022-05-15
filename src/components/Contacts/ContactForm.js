3edf9f7a3f3f49695bb8e7c2906fc4f1bfe11686
adolenec <andrija.dolenec23@gmail.com>
2022-05-15 14:55:43 +0200

    Rename css file

--- src/components/Contacts/ContactForm.js
+++ src/components/Contacts/ContactForm.js
@@ -1,4 +1,4 @@
-import classes from "./ContactsForm.module.css";
+import classes from "./ContactForm.module.css";
 import useInput from "../../hooks/useInput";
 import { useState } from "react";
 import { useSelector } from "react-redux";
